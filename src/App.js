2a98f0805ce42433e3cb599830f15b51a4a7d6ea
Gregory Arnheiter <gmarnheiter@gmail.com>
2021-10-23 18:53:19 -0700

    Added one user and login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Login from './components/login';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  const [page, setPage] = useState("LOGIN");
+
+
+  const successfulLoginHandler = (user) => {
+    setUser(user);
+  }
+
+  let pageToRender;
+
+  switch(page) {
+    case "LOGIN" :
+      pageToRender = <Login successfulLoginHandler = { successfulLoginHandler } /> 
+      break;
+    default:
+      pageToRender = <Login successfulLoginHandler = { successfulLoginHandler } />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Welcome To the BSC Website</h1>
+      { pageToRender }
+      
     </div>
   );
 }
